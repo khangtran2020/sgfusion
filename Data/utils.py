8972e1649b911cbbce866a2797d646b10901648f
khangtran2020 <kt36@njit.edu>
2024-06-06 22:45:59 +0300

    MCFL

--- Data/utils.py
+++ Data/utils.py
@@ -2,12 +2,13 @@ import os
 
 
 def read_data(data_path: str, country: str):
-    print("Country is:", country)
+    # print("Country is:", country)
     list_of_client = [
         "_".join(f.split("_")[:-1])
         for f in os.listdir(data_path)
         if (country in f) and ("train" in f)
     ]
+    print(list_of_client)
     client_dict = {}
     for i, f in enumerate(list_of_client):
         client_dict[i] = {"cid": i, "path": os.path.join(data_path, f)}
